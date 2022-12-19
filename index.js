b02944177e12e3db17f5ed0cee0146ce203a0a5c
Rafal Wartanowicz <32432397+rwart@users.noreply.github.com>
2022-12-19 23:01:59 +0100

    post message, updated

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 window.send = function() {
-    var iframe = document.getElementById('jsfiddle-frame').contentWindow;
+    var iframe = document.getElementById('frame-id').contentWindow;
     var message = document.querySelector('#textOutput').value;
     iframe.postMessage(message, '*');
 }
